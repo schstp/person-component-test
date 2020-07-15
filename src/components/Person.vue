1bddc3f3669fa6e1e09cc7b811a2182f7faf4de3
Stanislav <schstp@gmail.com>
2020-07-15 19:30:17 +0700

    home view and person-component made adaptive

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,14 +1,27 @@
 <template>
   <div class="container">
     <div class="container__column">
+      <div class="user-icon-wrapper">
+        <img src="../assets/img/person.png" alt="edit-icon" style="width: 30px">
+      </div>
+    </div>
+    <div class="container__column">
       {{ person.getFirstName() }}
     </div>
     <div class="container__column">
       {{ person.getLastName() }}
     </div>
     <div class="container__column">
-      <span>Edit</span>
-      <span>Delete</span>
+      <div class="control-btn-wrapper">
+        <button class="edit">
+          <img src="../assets/img/edit.svg" alt="edit-icon">
+        </button>
+      </div>
+      <div class="control-btn-wrapper">
+        <button class="delete">
+          <img src="../assets/img/remove.svg" alt="remove-icon">
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -30,16 +43,63 @@ export default {
 <style scoped lang="scss">
   .container {
     display: flex;
-    height: 30px;
+    height: 40px;
     padding: 0 15px;
     margin: 0 auto;
     justify-content: space-between;
     align-items: center;
 
+    border-bottom: 1px solid #e7e7e7;
+
     &__column {
       flex: 1;
+      &:first-of-type {
+        display: none;
+        @media screen and (min-width: 860px){
+          display: block;
+          flex: 0.2;
+        }
+      }
       &:last-of-type {
-        text-align: end;
+        display: flex;
+        justify-content: flex-end;
+      }
+      .control-btn-wrapper, .user-icon-wrapper {
+        width: 25px;
+        height: 25px;
+        margin: 0 3px;
+        @media screen and (min-width: 700px) {
+          width: 30px;
+          height: 30px;
+        }
+        button {
+          padding: 0;
+          border: 1px solid;
+          border-radius: 2px;
+          outline: none;
+          width: inherit;
+          height: inherit;
+          &.edit {
+            background-color: #fff2cc;
+            border-color: #dec578;
+            &:hover {
+              background-color: #fff1b3;
+            }
+          }
+          &.delete {
+            background-color: #f8cecc;
+            border-color: #c36e6b;
+            &:hover {
+              background-color: #f8bbb8;
+            }
+          }
+          &:hover {
+            cursor: pointer;
+          }
+        }
+        img {
+          width: 100%;
+        }
       }
     }
   }
--- src/views/home.vue
+++ src/views/home.vue
@@ -2,6 +2,7 @@
   <div v-if="!loadingState" class="container">
     <div class="table">
       <div class="table__header">
+        <div class="table__header__column"></div>
         <div class="table__header__column">Имя</div>
         <div class="table__header__column">Фамилия</div>
         <div class="table__header__column"></div>
@@ -13,7 +14,13 @@
           :person="person"
         />
       </div>
-      <div class="table__footer"></div>
+      <div class="table__footer">
+        <div class="table__footer__column">
+        </div>
+        <div class="table__footer__column">
+          <button>Добавить сотрудника</button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -55,23 +62,19 @@ export default {
     margin: 0 auto;
     padding: 0 15px;
     max-width: 1270px;
-    @media screen and (min-width: 500px){
-      width: 90%;
-    }
-    @media screen and (min-width: 800px){
-      width: 80%;
+    font-size: 14px;
+    @media screen and (min-width: 700px){
+      width: 95%;
+      font-size: 16px;
     }
     @media screen and (min-width: 1200px){
       width: 85%;
     }
-    @media screen and (min-width: 1500px){
-      width: 70%;
-    }
-    @media screen and (min-width: 1800px){
-      width: 65%;
+    @media screen and (min-width: 1600px){
+      width: 80%;
     }
     @media screen and (min-width: 2100px){
-      width: 55%;
+      width: 75%;
     }
   }
 
@@ -81,21 +84,64 @@ export default {
 
     &__header {
       display: flex;
-      height: 30px;
+      height: 40px;
       width: inherit;
       padding: 0 15px;
       margin: 0 auto;
       justify-content: space-between;
       align-items: center;
 
-      background-color: #ececec;
-      border: 1px solid #979797;
+      background-color: #f8f8f8;
+      border: 1px solid #e7e7e7;
       border-left: none;
       border-right: none;
       font-weight: bold;
 
       &__column {
         flex: 1;
+        &:first-of-type {
+          display: none;
+          @media screen and (min-width: 860px){
+            display: block;
+            flex: 0.2;
+          }
+        }
+      }
+    }
+
+    &__footer {
+      display: flex;
+      height: 50px;
+      width: inherit;
+      padding: 0 15px;
+      margin: 0 auto;
+      justify-content: flex-end;
+      align-items: center;
+
+      &__column {
+        flex: 3;
+        &:first-of-type {
+          display: none;
+          @media screen and (min-width: 860px){
+            display: block;
+            flex: 0.2;
+          }
+        }
+        display: flex;
+        justify-content: center;
+        @media screen and (min-width: 425px){
+          display: block;
+        }
+        button {
+          height: 40px;
+          background-color: #3d8bcd;
+          color: #ffffff;
+          border: none;
+          border-radius: 5px;
+          outline: none;
+          font-size: inherit;
+          font-weight: bold;
+        }
       }
     }
   }
