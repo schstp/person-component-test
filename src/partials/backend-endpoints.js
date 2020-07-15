864d4a8a6e0091d09995b894de0c422db51ad201
Stanislav <schstp@gmail.com>
2020-07-15 12:56:16 +0700

    db.json & routes.json for json-server defined and tested in Postman

--- src/partials/backend-endpoints.js
+++ src/partials/backend-endpoints.js
@@ -1,7 +1,7 @@
 export const BACKEND_ENDPOINTS = {
   getPersons: { method: 'get', url: 'persons' },
   getPerson: { method: 'get', url: 'person/{personId}' },
-  updatePerson: { method: 'put', url: 'person' },
+  updatePerson: { method: 'put', url: 'person/{personId}' },
   addPerson: { method: 'post', url: 'person/{personId}' },
   deletePerson: { method: 'delete', url: 'person/{personId}' },
 }
